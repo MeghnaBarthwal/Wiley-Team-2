22d3bee5df87c9c105b83019a2269b4e5276722c
meghnabarthwal <meghnabarthwal123@gmail.com>
2022-11-30 20:43:34 +0530

    added emp2 bean;

--- SpringDemo1/src/com/beans/EmpApp.java
+++ SpringDemo1/src/com/beans/EmpApp.java
@@ -17,9 +17,9 @@ public interface EmpApp {
 		ApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"emp2.xml","emp.xml"});
 		System.out.println("Application context loaded !!");
 		Employee e1 = (Employee)context.getBean("emp1");
-		//Employee e2 = (Employee)context.getBean("emp2");
+		Employee e2 = (Employee)context.getBean("emp2");
 		System.out.println(e1);
-		//System.out.println(e2);
+		System.out.println(e2);
 		
 //		ApplicationContext context = new ClassPathXmlApplicationContext("emp.xml");
 //		System.out.println("Application context loaded !!");
